af1c06453ce844a9dc4fc69daf75084d6c5f7079
c0a2403168 <c0a2403168@edu.teu.sc.jp>
2025-09-30 15:40:32 +0900

    演習１こうかとん画像とゲームオーバーの表示

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
-
+import time
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -14,6 +14,30 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def gameover(screen: pg.Surface) -> None:
+    """
+    ゲームオーバーを表示する
+    画面中央に「GAME OVER」と表示する
+    五秒間表示した後、プログラムを終了する
+    """
+    gameover_sfc = pg.Surface((WIDTH, HEIGHT)) #からのSurfaceを作成する
+    gameover_sfc.fill((0, 0, 0)) # 黒色で塗りつぶす
+    gameover_sfc.set_alpha(150) # 透明度を設定する
+    screen.blit(gameover_sfc, (0, 0)) # 画面に貼り付ける 
+    font = pg.font.Font(None, 100) # フォントのサイズ決め
+    text = font.render("GAME OVER", True, (255, 255, 255)) # ゲームオーバーを白色で表示
+    text_rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2)) # 画面中央に表示
+    screen.blit(text, text_rect)
+    gameover_kk_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 2.0) # 泣いたこうかとんの画像
+    gameover_kk_rect_1 = gameover_kk_img.get_rect(center=(WIDTH // 2 -300, HEIGHT // 2 )) # 画面左に表示
+    gameover_kk_rect_2 = gameover_kk_img.get_rect(center=(WIDTH // 2 +300, HEIGHT // 2 )) # 画面右に表示
+    screen.blit(gameover_kk_img, gameover_kk_rect_1)
+    screen.blit(gameover_kk_img, gameover_kk_rect_2)
+    pg.display.update() # 画面を更新する    
+    time.sleep(5) # 5秒間表示する
+
+
+    
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
     引数：こうかとんRect or 爆弾Rect
@@ -83,6 +107,8 @@ def main():
         if not tate: # 縦方向にはみ出していたら
             vy *= -1
         screen.blit(bb_img, bb_rct) # 赤い爆弾を画面に貼り付ける
+
+        gameover(screen) if kk_rct.colliderect(bb_rct) else None # こうかとんが赤い爆弾に当たったらゲームオーバ
 
         pg.display.update()
         tmr += 1
